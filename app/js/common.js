9970e31cea5d0053f33740268fc7c7094dbd0949
Davyd Tevzadze <pawtod20@gmail.com>
2020-05-12 15:24:57 +0300

    top line & contacts info

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,11 @@
 $(function() {
 
-
-
+	$('.more_contacts img').on('click' , function () {
+		$(this).toggleClass('activeArrow');
+		$('.more_contacts-inner').toggleClass('activeInfo');
+	})
+	$('.close').on('click', function () {
+		$('.more_contacts img').toggleClass('activeArrow');
+		$('.more_contacts-inner').removeClass('activeInfo');
+	} )
 });
